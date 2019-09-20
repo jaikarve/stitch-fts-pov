070ff4de701f4e35b13157c33274cacc2a4158be
Jai Karve <jai.karve@Jais-MacBook-Pro.local>
2019-09-20 08:49:40 -0700

    First commit

---
+++ services/mdb-atlas-pov-service/incoming_webhooks/mdb-pov-fts/source.js
@@ -0,0 +1,25 @@
+// This function is the webhook's request handler.
+exports = function(payload) {
+  const collection = context.services.get("mongodb-atlas-fts").db("sample_mflix").collection("movies");
+  
+  	let arg = payload.query.arg;
+
+  	return collection.aggregate([
+  	  {$searchBeta: {
+        search: {
+          path: 'fullplot',
+          query: arg
+        }
+      }}, 
+      {$project: {
+        title: 1,
+        _id: 0,
+        year: 1,
+        fullplot: 1,
+        score: {$meta: 'searchScore'}
+        
+      }},
+      {$limit:
+        10
+      }]).toArray();
+};
